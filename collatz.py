23a4b35323371ef522b68ae7880a8bc95e1e88ce
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-08-19 16:15:57 -0600

    Create collatz()

--- collatz.py
+++ collatz.py
@@ -1,1 +1,10 @@
-
+def collatz(number):
+    """
+    Applies the Collatz conjecture formula to a given number.
+    
+    Args:
+        number (int): The input number to process
+        
+    Returns:
+        int: The result of the Collatz operation
+    """
